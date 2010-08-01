15b63371fbc7b358823d32c279c88ecd398404e3
Claudio Braga Leite <claudiobl2010@hotmail.com>
2010-08-01 03:17:25 -0300

    continuando fluxo de cadastro

--- controller/time.py
+++ controller/time.py
@@ -4,9 +4,25 @@
 import cherrypy
 
 from superconfronto.controller.base import *
+from datetime import datetime
+from superconfronto.models.clube import Clube
 
 class TimeController(BaseController):
     
     @logged
     def cadastro(self, game):
-        return self.render_to_template("cadastro.html")
+        
+        contexto = cherrypy.session.get('contexto')
+        
+        time_cartola = contexto.get('time_cartola')
+        time = contexto.get('time')
+        
+        temporada = datetime.now().strftime('%Y')
+        
+        clube = Clube().get(time_cartola['clube_id'])
+
+        return self.render_to_template("cadastro.html", 
+                                       time_cartola=time_cartola, 
+                                       time=time, 
+                                       temporada=temporada, 
+                                       clube=clube)
