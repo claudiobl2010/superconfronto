a896c9aec15da13ad2a021b5ae8612bf8e053ba2
Claudio Braga Leite <claudiobl2010@hotmail.com>
2010-08-25 01:52:25 -0300

    avancando com a implementacao da cortina de mensagem

--- media/js/message.js
+++ media/js/message.js
@@ -2,7 +2,13 @@ function showMessage(opts) {
 
 	var box_msg = "";
 	box_msg += "<div class='sc-box-msg'>";
-	box_msg += "alow";
+	box_msg += "<div class='sc-msg-fechar'>";
+	box_msg += "<a href='#'><img src='/media/img/msg_fechar.png'></a>";
+	box_msg += "</div>";
+	box_msg += "<div class='sc-msg-content'>";
+	box_msg += "<img src='/media/img/msg_sucesso.png'>";
+	box_msg += '<strong>Seu time foi escalado com sucesso!</strong>';	
+	box_msg += "</div>";
 	box_msg += "</div>";
 	
     $(box_msg).appendTo($("#sc-doc"));
